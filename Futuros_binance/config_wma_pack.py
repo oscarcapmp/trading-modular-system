737a6aff03e5925501bdb336c8fdc2c2e453430e
oscarcapmp <oscar.cardenas.a@hotmail.com>
2025-12-26 21:51:29 -0500

    pack hasta WMA camaleona

--- Futuros_binance/config_wma_pack.py
+++ Futuros_binance/config_wma_pack.py
@@ -6,7 +6,6 @@ WMA_CARMESI_LEN = 233
 WMA_BLANCA_LEN = 377
 WMA_LIMA_LEN = 610
 WMA_CAMALEONA_LEN = 987
-WMA_MORADA_LEN = 1597
 
 WMA_PACK_ORDER = [
     ("Pollita", WMA_POLLITA_LEN),
@@ -17,7 +16,6 @@ WMA_PACK_ORDER = [
     ("Blanca", WMA_BLANCA_LEN),
     ("Lima", WMA_LIMA_LEN),
     ("Camaleona", WMA_CAMALEONA_LEN),
-    ("Morada", WMA_MORADA_LEN),
 ]
 
 MAX_WMA_PACK_LEN = max(length for _, length in WMA_PACK_ORDER)
